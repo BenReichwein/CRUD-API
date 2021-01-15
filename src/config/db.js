3f266d141b9f9064f581943e80209b740b832143
Ben <47262607+BenReichwein@users.noreply.github.com>
2021-01-14 22:37:24 -0500

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const db = async () => {
     try {
         await mongoose.connect(
-            'mongodb+srv://Ben:gamingmaster4224@test.evxl7.mongodb.net/test?retryWrites=true&w=majority',
+            'MONGO_KEY',
             { useNewUrlParser: true },
             { useUnifiedTopology: true },
             () => console.log('[CONNECTED] - Database'))
